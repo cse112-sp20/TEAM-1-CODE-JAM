9c3c30120b68ddacf2267dd2ec2672bb437834dd
Karl Wang <kawang@ucsd.edu>
2020-05-23 21:39:39 -0700

    style: shrink the size of navbar and add the redirect setting page option

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,4 @@
+/*global chrome*/
 import React, { Component } from "react";
 import M from "materialize-css";
 import "./NavBar.css";
@@ -7,18 +8,22 @@ export default class NavBar extends Component {
     M.AutoInit();
   }
 
+  openSettingPage = () => {
+    chrome.tabs.create({ url: "/popup.html" });
+  };
+
   render() {
     return (
       <div>
         {/* <!-- Navbar goes here --> */}
         <nav id="nav-background">
           <div className="nav-wrapper">
-            <a href="#" className="brand-logo">
-              Team 1
+            <a id="team-name" className="left">
+              Team Activity Tracker
             </a>
             <ul className="right">
               <li>
-                <a href="#">About</a>
+                <a onClick={this.openSettingPage}>Settings</a>
               </li>
             </ul>
           </div>
