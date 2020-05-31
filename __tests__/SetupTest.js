1292abdf657a7dcd931b6e334db531c195595ea5
hwanggit <luw055@ucsd.edu>
2020-05-31 16:14:26 -0700

    merged develop

--- __tests__/SetupTest.js
+++ __tests__/SetupTest.js
@@ -1,12 +1,19 @@
-import React from "react";
-import "babel-polyfill";
-import App from "../src/App";
-import renderer from "react-test-renderer";
-const chrome = require("sinon-chrome");
-window.chrome = chrome;
+import '@testing-library/jest-dom'
+import React from 'react';
+import {render, fireEvent, screen} from '@testing-library/react';
+import App from '../src/App';
 
-test("App is set up correctly", () => {
-  const component = renderer.create(<App />);
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
+test('Check that app is set up correctly.', () => {
+  render(<App />)
+
+  // query* functions will return the element or null if it cannot be found
+  // get* functions will return the element or throw an error if it cannot be found
+  // expect(screen.queryByText("Timeline")).toBeNull()
+
+  // the queries can accept a regex to make your selectors more resilient to content tweaks and changes.
+  // fireEvent.click(screen.getByLabelText(/show/i))
+
+  // .toBeInTheDocument() is an assertion that comes from jest-dom
+  // otherwise you could use .toBeDefined()
+  // expect(screen.getByText(testMessage)).toBeInTheDocument()
+})
