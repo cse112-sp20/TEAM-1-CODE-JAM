5fc35e843dee829a68c3d78bf3bc839ccfbd7c4d
hwanggit <luw055@ucsd.edu>
2020-06-04 14:34:24 -0700

    tests/chrome-runtime-mock_howard

--- __mocks__/chromeMock.js
+++ __mocks__/chromeMock.js
@@ -1,6 +1,9 @@
 // Define a tabs array
 var openTabs = [];
 
+// Define teams array
+var teams = [];
+
 // Push a new tab
 const pushTabs = (data) => {
   openTabs.push(data);
@@ -25,14 +28,27 @@ let runtime = {
   sendMessage: (msgObj, callback) => {
     const { for: sendTo, message } = msgObj;
 
-    
+    // If get teams, then get a fake team
+    if (sendTo === "background" && message === "get teams") {
+      teams.push({
+        visable: true,
+        teamCode: "TEST_TEAM_CODE",
+        teamName: "TEST_TEAM",
+      });
 
-  }
+      // Return teams to runtime
+      callback(teams);
+    }
+    // Otherwise return undefined
+    else {
+      callback(undefined);
+    }
+  },
 };
 
 // Export chrome
 export const chrome = {
   identity: identity,
   tabs: tabs,
-  runtime: 
+  runtime: runtime,
 };
