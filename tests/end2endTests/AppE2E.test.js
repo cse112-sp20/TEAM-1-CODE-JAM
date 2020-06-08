3f481b3b680d078ea83653bfccfd0e5202b56d23
trevor <t1perez@ucsd.edu>
2020-06-07 19:44:36 -0700

    fix: Integrating pupetteer
    Keep all the e2e functionality in one file

--- tests/end2endTests/AppE2E.test.js
+++ tests/end2endTests/AppE2E.test.js
@@ -1,10 +1,108 @@
-import chrome from "sinon-chrome";
-global.chrome = chrome;
-// const puppeteer = require("puppeteer");
+import path from 'path'
+import puppeteer from 'puppeteer'
+import manifest from '../../build/manifest.json'
 
-describe("delete later and uncomment the following", () => {
-  test("delete later", () => {});
-});
+const TEST_TIMEOUT = 20000 // extend test timeout sinces its E2E
+
+let browser
+let page
+const BUILD_PATH = path.resolve(__dirname, '../../build')
+
+let extensionId = null
+
+const getExtensionId = async () => {
+  const dummyPage = await browser.newPage()
+  await dummyPage.waitFor(2000) // arbitrary wait time.
+
+  const targets = await browser.targets()
+  const extensionTarget = targets.find(
+    ({ _targetInfo }) =>
+      _targetInfo.title === manifest.name &&
+      _targetInfo.type === 'background_page'
+  )
+  // eslint-disable-next-line no-underscore-dangle
+  const extensionUrl = extensionTarget._targetInfo.url || ''
+  const [, , extensionID] = extensionUrl.split('/')
+  dummyPage.close()
+  return extensionID
+}
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.PUPPETEER_EXEC_PATH,
+    headless: false,
+    args: [
+      `--disable-extensions-except=${BUILD_PATH}`,
+      `--load-extension=${BUILD_PATH}`,
+      '--no-sandbox',
+    ],
+    defaultViewport: {
+      width: 1280,
+      height: 800,
+    },
+    slowMo: 100,
+  })
+
+  extensionId = await getExtensionId()
+})
+
+afterAll(async () => {
+  if (browser) {
+    await browser.close()
+  }
+})
+
+beforeEach(async () => {
+  page = await browser.newPage()
+})
+
+afterEach(async () => {
+  if (page) {
+    await page.close()
+  }
+})
+
+
+test(
+  'newtab page should have a player',
+  async () => {
+    await page.goto(`chrome-extension://${extensionId}/index.html`)
+    //await waitDOMLoaded()
+    const el = 'test'
+    await page.waitFor(500);
+    expect(el).not.toBe(null)
+  },
+  TEST_TIMEOUT
+)
+
+// // TODO need a good indicator that deeplinking is working
+// test(
+//   'newtab page should be able to deeplink into a exercise',
+//   async () => {
+//     await page.goto(
+//       `chrome-extension://${extensionId}/index.html?play=true`
+//     )
+//     await waitDOMLoaded()
+//     await wait(1000)
+//     const el = await page.$('[data-testid="breath-player--count"]')
+//     expect(el).not.toBe(null)
+
+//     const initialCount = await (await el.getProperty(
+//       'textContent'
+//     )).jsonValue()
+//     expect(initialCount.trim()).toBe('5')
+//   },
+//   TEST_TIMEOUT
+// )
+
+
+// import chrome from "sinon-chrome";
+// global.chrome = chrome;
+// // const puppeteer = require("puppeteer");
+
+// describe("delete later and uncomment the following", () => {
+//   test("delete later", () => {});
+// });
 // const extensionPath = "./build";
 
 // const extensionUrl = "chrome-extension://imdkakgonmilneihbfjnlfbjgbidmldj";
